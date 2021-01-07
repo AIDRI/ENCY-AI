072395cb18dcc51993ff1c04929ff009ea6f56c6
AIDRI <60047183+AIDRI@users.noreply.github.com>
2021-01-07 17:26:25 +0100

    Update and rename r.md to download.sh

---
+++ checkpoints/download.sh
@@ -0,0 +1,2 @@
+wget -O "checkpoints/bert.pt" "https://www.googleapis.com/drive/v3/files/1t27zkFMUnuqRcsqf2fh8F1RwaqFoMw5e?alt=media&key=AIzaSyCmo6sAQ37OK8DK4wnT94PoLx5lx-7VTDE"
+wget -O "checkpoints/distilbert.pt" "https://www.googleapis.com/drive/v3/files/1WxU7cHECfYaU32oTM0JByTRGS5f6SYEF?alt=media&key=AIzaSyCmo6sAQ37OK8DK4wnT94PoLx5lx-7VTDE"
