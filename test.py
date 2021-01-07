7f2d8260d2f49c08c6a6a9e346e78d464aa182dd
AIDRI <60047183+AIDRI@users.noreply.github.com>
2021-01-07 17:49:27 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,10 @@ from models.sum import predict, load_text, preprocessing
 from models.model import build_model
 
 print('Loading Model')
-model = build_model(checkpoint=torch.load(f'checkpoints/bert.pt', map_location='cpu'), bert_type='bert', device='cpu')
+
+model='bert' #
+
+model = build_model(checkpoint=torch.load(f'checkpoints/{model}.pt', map_location='cpu'), bert_type=model, device='cpu')
 
 print('Try to summarize the text')
 predict(model, load_text(preprocessing('test/inp.txt'), 512), 'test/out.txt', 10)
