c0f60a6a32295f28b6d4317176d05619d8fb3ad8
AIDRI <ad.israel38@gmail.com>
2021-01-10 20:54:12 +0100

    add wikipedia website search

---
+++ AI/wiki.py
@@ -0,0 +1,14 @@
+import wikipedia
+
+def search_on_wikipedia(keywords):
+	websites = []
+	tmp = ''
+
+	for i in range(len(keywords)-1):
+		tmp = keywords[i] + ' ' + keywords[i+1]
+		tmp = wikipedia.search(tmp)[:2]
+		for c in tmp:
+			if c not in websites:
+				websites.append(c)
+	
+	return websites
