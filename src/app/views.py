781f9a1ec9fcc74e57d3a2484c9dc235d0aa61df
milan090 <milanmuhammed1@gmail.com>
2021-01-24 17:50:07 +0530

    few spelling corrections

--- src/app/views.py
+++ src/app/views.py
@@ -30,7 +30,7 @@ def chatterReq():
 	return out
 
 
-@app.route('/summary', methods=['POST'])
+@app.route('/summarize-text', methods=['POST'])
 def summary():
 	if not request.json:
 		return { "error": "No json body found in request" }
@@ -56,7 +56,7 @@ def summary():
 	return out
 
 
-@app.route('/summarise-url', methods=["POST"])
+@app.route('/summarize-url', methods=["POST"])
 def summarise_url():
 	if not request.json:
 		return { "error": "No json body found in request" }
