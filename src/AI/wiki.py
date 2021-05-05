3341264b6e6f639844a3e688a405473a7bfd5340
AIDRI <ad.israel38@gmail.com>
2021-05-05 22:02:31 +0200

    updates

--- src/AI/wiki.py
+++ src/AI/wiki.py
@@ -3,9 +3,8 @@ import wikipedia
 def search_on_wikipedia(keywords, lang):
 	websites = []
 	tmp = ''
-	keywords.append([' '])
 	for i in range(len(keywords)-1):
-		tmp = keywords[i] + ' ' + keywords[i+1]
+		tmp = str(keywords[i] )+ ' ' + str(keywords[i+1])
 		tmp = wikipedia.search(tmp)[:2]
 		for c in tmp:
 			if c not in websites:
